b70b17f68c8f68f498e21c73e684fb04a4fdeacd
SANJEET <167337847+sanjeetjangid00@users.noreply.github.com>
2025-12-24 14:00:20 +0530

    Change st.write_stream to st.markdown for output

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if chat:
             yield word + " "
             time.sleep(0.02)
     for text in stream_data(response_text):
-        st.write_stream(
+        st.markdown(
             f'<div style="text-align: left; background-color: #E0F7FA; border-radius: 15px; padding: 10px; max-width: 110%; margin: 10px 0; display: inline-block;">'
             f'<img src="https://img.icons8.com/ios/452/artificial-intelligence.png" style="vertical-align: middle; width: 25px; height: 25px; margin-right: 10px;" />'
             f'{text}</div>',
